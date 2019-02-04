ae7000d7f7009b88ee5f2f4d925fe4c7d9cbddb6
ShubhamYGS <s.patil14569@gmail.com>
2019-02-04 17:57:07 +0530

    Initial commit

---
+++ app/src/main/java/quiz1/learnjava/emailsender1/MainActivity.java
@@ -0,0 +1,55 @@
+package quiz1.learnjava.emailsender1;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etContent, etRecipient, etSubject;
+    Button btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etSubject = (EditText)findViewById(R.id.etSubject);
+        etContent = (EditText) findViewById(R.id.etContent);
+        etRecipient = (EditText)findViewById(R.id.etRecipient);
+        btnSend = (Button) findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setTitle("Sending Email");
+        dialog.setMessage("Please wait");
+        dialog.show();
+        Thread sender = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    GMailSender sender = new GMailSender("yourmail@gmail.com", "password");
+                    sender.sendMail(etSubject.getText().toString(),
+                            etContent.getText().toString(),
+                            "yourmail@gmail.com",
+                            etRecipient.getText().toString());
+                    dialog.dismiss();
+                } catch (Exception e) {
+                    Log.e("mylog", "Error: " + e.getMessage());
+                }
+            }
+        });
+        sender.start();
+    }
+}
